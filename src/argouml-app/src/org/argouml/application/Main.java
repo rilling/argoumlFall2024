cedb6f302f6ffe999a966d8738caccf7acda6db4
thuhinkhanna <qor6119@gmail.com>
2024-10-03 04:09:13 -0400

    Removed unused block of commented-out code

--- src/argouml-app/src/org/argouml/application/Main.java
+++ src/argouml-app/src/org/argouml/application/Main.java
@@ -749,38 +749,7 @@ public class Main {
          *  If it is set, then we let the static initializer in
          * {@link Argo} perform the initialization.
          */
-
-        // JavaWebStart properties for logs are :
-        // deployment.user.logdir & deployment.user.tmp
-//        if (System.getProperty("log4j.configuration") == null) {
-//            Properties props = new Properties();
-//            InputStream stream = null;
-//            try {
-//                stream = Thread.currentThread().getContextClassLoader()
-//                        .getResourceAsStream(DEFAULT_LOGGING_CONFIGURATION);
-//
-//                if (stream != null) {
-//                    props.load(stream);
-//                }
-//            } catch (IOException io) {
-//                io.printStackTrace();
-//                System.exit(-1);
-//            }
-//
-//            PropertyConfigurator.configure(props);
-//
-//            if (stream == null) {
-//                BasicConfigurator.configure();
-//                Logger.getRootLogger().getLoggerRepository().setThreshold(
-//                        Level.ERROR); // default level is DEBUG
-//                Logger.getRootLogger().error(
-//                        "Failed to find valid log4j properties"
-//                        + "in log4j.configuration"
-//                        + "using default logging configuration");
-//            }
-//        }
-
-        // initLogging();
+        
         LOG = Logger.getLogger(Main.class.getName());
     }
 
